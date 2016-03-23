6a8abb8fb8de3de8a0550de1ed76364ca04cdade
johannes karoff <johannes@karoff.net>
2016-03-23 12:06:23 +0100

    update

--- src/filter.rs
+++ src/filter.rs
@@ -11,16 +11,24 @@ pub struct RedGreenFilter {
     k3: i32,
 }
 
-fn rgb2lin(rgb: f32) -> i32 {
-    (rgb * 32767.0) as i32
+pub struct LinTriple {
+    r: i32,
+    g: i32,
+    b: i32,
+}
+
+fn rgb2lin(rgb: Rgb) -> LinTriple {
+    LinTriple {
+        r: (rgb.red * 32767.0) as i32,
+        g: (rgb.green * 32767.0) as i32,
+        b: (rgb.blue * 32767.0) as i32,
+    }
 }
 
 impl ColorFilter for RedGreenFilter {
     fn transform(&self, rgb: Rgb) -> Rgb {
         // get linear rgb values in the range 0..2^15-1
-        let r_lin = rgb2lin(rgb.red);
-        let g_lin = rgb2lin(rgb.green);
-        let b_lin = rgb2lin(rgb.blue);
+        let lin = rgb2lin(rgb);
 
         // simulated red and green are identical
         // scale the matrix values to 0..2^15 for integer computations 
@@ -29,17 +37,17 @@ impl ColorFilter for RedGreenFilter {
         // also divide by 2^15 and multiply by 2^8 to scale the linear rgb to 0..255
         // total division is by 2^15 * 2^15 / 2^8 = 2^22
         // shift the bits by 22 places instead of dividing
-        let r_blind = (self.k1 * r_lin + self.k2 * g_lin) >> 22;
-        let b_blind = (self.k3 * r_lin - self.k3 * g_lin + 32768 * b_lin) >> 22;
+        let r_blind = (self.k1 * lin.r + self.k2 * lin.g) >> 22;
+        let b_blind = (self.k3 * lin.r - self.k3 * lin.g + 32768 * lin.b) >> 22;
 
-        let red = cmp::min(0, cmp::max(255, r_blind));
-        let blue = cmp::min(0, cmp::max(255, b_blind));
+        let red = cmp::max(0, cmp::min(255, r_blind));
+        let blue = cmp::max(0, cmp::min(255, b_blind));
 
         // convert reduced linear rgb to gamma corrected rgb
-        let red = if red >= 0 { red } else { 256 + red }; // from unsigned to signed
-        let blue = if blue >= 0 { blue } else { 256 + blue }; // from unsigned to signed
+        let red: i32 = if red >= 0 { red } else { 256 + red }; // from unsigned to signed
+        let blue: i32 = if blue >= 0 { blue } else { 256 + blue }; // from unsigned to signed
 
-        let out = Rgb::new_u8(((red << 16) >> 16) as u8, ((red << 8) >> 8) as u8, blue as u8);
+        let out = Rgb::new_u8(red as u8, red as u8, blue as u8);
 
         out
     }
